6d1f1b371cf35453eb065b80a45c5b60f556fdf0
setokk <tesmtracks@gmail.com>
2025-05-05 00:32:49 +0300

    Added pipeline for validating card combinations (player moves)

---
+++ MuLiveGameServer/src/main/java/org/murlan/live/game/logic/MovePipeline.java
@@ -0,0 +1,45 @@
+package org.murlan.live.game.logic;
+
+import org.murlan.live.game.deck.CardCombination;
+import org.murlan.live.game.logic.classifier.CardCombinationClassifier00;
+import org.murlan.live.game.logic.classifier.CardCombinationClassifier01;
+import org.murlan.live.game.logic.classifier.CardCombinationClassifier02;
+import org.murlan.live.game.logic.classifier.CardCombinationClassifier03;
+import org.murlan.live.game.logic.classifier.CardCombinationClassifier04;
+import org.murlan.live.game.logic.classifier.CardCombinationClassifier05;
+import org.murlan.live.game.logic.classifier.ICardCombinationClassifier;
+
+import java.util.HashSet;
+
+/**
+ * Pipeline to be executed whenever a hand is attempted to be played
+ */
+public class MovePipeline {
+    private static final ICardCombinationClassifier[] cardCombinationClassifiers = new ICardCombinationClassifier[] {
+            new CardCombinationClassifier00(),
+            new CardCombinationClassifier01(),
+            new CardCombinationClassifier02(),
+            new CardCombinationClassifier03(),
+            new CardCombinationClassifier04(),
+            new CardCombinationClassifier05()
+    };
+
+    public static boolean isMoveValid(CardCombination cardCombination) {
+        if (containsDuplicates(cardCombination)) {
+            return false;
+        }
+        for (ICardCombinationClassifier cardCombinationClassifier : cardCombinationClassifiers) {
+            boolean isClassified = cardCombinationClassifier.classifyCardCombination(cardCombination);
+            if (isClassified) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean containsDuplicates(CardCombination cardCombination) {
+        int numCardsReceived = cardCombination.getCards().size();
+        int numDistinctCards = new HashSet<>(cardCombination.getCards()).size();
+        return numCardsReceived != numDistinctCards;
+    }
+}
---
+++ MuLiveGameServer/src/test/java/org/murlan/live/game/logic/MovePipelineTest.java
@@ -0,0 +1,28 @@
+package org.murlan.live.game.logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.murlan.live.game.deck.Card;
+import org.murlan.live.game.deck.CardCombination;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MovePipelineTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovePipelineTest.class);
+
+    @Test
+    public void testDoubleCards() {
+        CardCombination cardCombination = new CardCombination(Card.KING_OF_HEARTS, Card.KING_OF_DIAMONDS);
+        LOGGER.debug("testDoubleCards() -> Card combination: {{}} should be valid!", cardCombination);
+        boolean isMoveValid = MovePipeline.isMoveValid(cardCombination);
+        Assert.assertTrue(isMoveValid);
+    }
+
+    @Test
+    public void testDuplicateCards() {
+        CardCombination cardCombination = new CardCombination(Card.QUEEN_OF_HEARTS, Card.QUEEN_OF_HEARTS);
+        LOGGER.debug("testDuplicateCards() -> Card combination: {{}} should NOT be valid!", cardCombination);
+        boolean isMoveValid = MovePipeline.isMoveValid(cardCombination);
+        Assert.assertFalse(isMoveValid);
+    }
+}
